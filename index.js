ca7fc9e9e8b77e6e399ceff6cae93b284b1b375c
Eliza-collab <hanenbadradam@gmail.com>
2026-09-16 18:38:23 +0200

    Initial Node.js project

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("My name is Fatima Adam.");
+console.log("I'm learning Node.js.");
+console.log("Day 1 is completed!.");
+console.log("My favorite programming languages are: JavaScript & C#.");
+console.log("I want to learn Software Development to improve my practical skills.");
